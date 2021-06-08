1d5f65691f21de067d6fbe20e8212255b53f3fb5
Shivani <pathaksm99@gmail.com>
2021-06-08 17:22:54 +0530

    commit

---
+++ 1/dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "internet_hunters";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
---
+++ 2/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "internet_hunters";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>
